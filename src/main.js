3e067ef2eae23d2b52ff4de1baaea7617eba66b3
dsadog <1739988739@qq.com>
2022-12-10 17:40:55 +0800

    完成首页、导航、

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import './assets/css/global.css'
+
+
+createApp(App).use(router).mount('#app')
